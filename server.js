7a66b361021e3e68395138dde7572f0ad35c6fe6
Amelia Roberts <arob0017@student.monash.edu>
2020-12-03 16:15:09 +1100

    added html and api routes to server

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
     useNewUrlParser: true,
     useFindAndModify: false
 });
-
+const db = require("./models");
 // routes
-app.use(require("./routes/api.js"));
+require("./routes/api-routes")(app);
+require("./routes/html-routes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
